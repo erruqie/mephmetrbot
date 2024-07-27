541bdaa94eac5167c9301b06df4b64f529c0a606
Maxim <clownl3ss@icloud.com>
2024-07-27 04:26:29 +0400

    cryptopay fixes =)

--- mephmetrbot/handlers/cryptopay.py
+++ mephmetrbot/handlers/cryptopay.py
@@ -25,7 +25,6 @@ async def buymeph_command(message: Message):
     await Invoices.create(invoice_id=invoice['invoice_id'], user_id=message.from_user.id, amount_ton=0.2, amount_meph=100, status='active')
 
 async def checkinvoice(invoice_id, message: Message):
-    await message.reply('kek')
     invoices = client.get_invoices(
         "TON",
         status="paid",
@@ -35,10 +34,16 @@ async def checkinvoice(invoice_id, message: Message):
         invoice_ids = invoice_id
     )
     if invoices:
-        invoice = await Invoices.get(invoice_id=invoice_id)
+        try:
+            invoice = await Invoices.get(invoice_id=invoice_id)
+        except:
+            await message.reply(f"❌ Этот счёт не найден")
+            return
         await message.reply(f"✅ Ты крут бро ты получил {invoice.amount_meph} грамм мефа, спасибо за поддержку!")
-        user = await Users.get(id={message.from_user.id})
-        user.drug_count += invoice.amount_meph
+        user = await Users.get(id=message.from_user.id)
+        new_balance = round(user.drug_count + invoice.amount_meph, 1)
+        user.drug_count = new_balance
         await user.save()
+        await Invoices.filter(invoice_id=invoice_id).delete()
     else:
         await message.reply(f"❌ Оплата не найдена")
