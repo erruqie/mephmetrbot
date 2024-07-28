c97c4a0dd0422c9afe3a48bfcdb57e7f5792ab41
Maxim <clownl3ss@icloud.com>
2024-07-29 02:16:37 +0400

    some fixes

--- mephmetrbot/handlers/cryptopay.py
+++ mephmetrbot/handlers/cryptopay.py
@@ -1,6 +1,6 @@
 from aiocryptopay import AioCryptoPay, Networks
 from mephmetrbot.handlers.models import Users, Invoices
-from mephmetrbot.config import bot, CRYPTO_PAY_TOKEN, RUB_PER_MEPH
+from mephmetrbot.config import bot, CRYPTO_PAY_TOKEN, RUB_PER_MEPH, LOGS_CHAT_ID
 from aiogram import Router
 from aiogram.types import Message, InlineKeyboardButton, CallbackQuery
 from aiogram.filters.command import Command, CommandObject
@@ -26,38 +26,40 @@ async def buymef(message: Message, command: CommandObject):
                             f'Пример: <code>/buymeph 100</code>\n\n'
                             f'Курс: <code>1 гр. = {float(RUB_PER_MEPH)} RUB</code>', parse_mode='html')
         return
-    if value < 10:
+    if int(value) < 10:
         await message.reply(f'❌ Минимальная сумма для покупки: 10 гр.')
         return
-    invoice_amount = int(int(value) * float(RUB_PER_MEPH))
-    invoice = await crypto.create_invoice(
-        amount=invoice_amount,
-        fiat='RUB',
-        currency_type='fiat',
-        description=f'{value} единиц игровой валюты',
-        allow_comments=True,
-        allow_anonymous=True,
-        expires_in='300'
-    )
+    else:
+        invoice_amount = int(int(value) * float(RUB_PER_MEPH))
+        invoice = await crypto.create_invoice(
+            amount=invoice_amount,
+            fiat='RUB',
+            currency_type='fiat',
+            description=f'{value} единиц игровой валюты',
+            allow_comments=True,
+            allow_anonymous=True,
+            expires_in='300'
+        )
 
-    builder = InlineKeyboardBuilder()
-    builder.row(
-        InlineKeyboardButton(text='💰 Оплатить', url=invoice.bot_invoice_url),
-        InlineKeyboardButton(text='Проверить оплату', callback_data=f"checkinvoice_{invoice.invoice_id}_{value}")
-    )
+        builder = InlineKeyboardBuilder()
+        builder.row(
+            InlineKeyboardButton(text='💰 Оплатить', url=invoice.bot_invoice_url),
+            InlineKeyboardButton(text='Проверить оплату', callback_data=f"checkinvoice_{invoice.invoice_id}_{value}_{message.from_user.id}")
+        )
 
-    await message.reply(
-        f'💰 Вы можете купить {value} гр. за {invoice_amount} RUB через CryptoBot\n\n',
-        parse_mode='markdown',
-        disable_webpage_preview=True,
-        reply_markup=builder.as_markup()
-    )
+        await message.reply(
+            f'💰 Вы можете купить {value} гр. за {invoice_amount} RUB через CryptoBot\n\n',
+            parse_mode='markdown',
+            disable_webpage_preview=True,
+            reply_markup=builder.as_markup()
+        )
 
 @router.callback_query(F.data.startswith("checkinvoice_"))
 async def checkinvoice_callback(callback: CallbackQuery):
     data = callback.data.split("_")
     invoice_id = data[1]
     value = data[2]
+    userid = data[3]
     try:
         invoice_data = await Invoices.get(invoice_id=invoice_id)
         status = invoice_data.status
@@ -68,13 +70,19 @@ async def checkinvoice_callback(callback: CallbackQuery):
         invoice = await crypto.get_invoices(invoice_ids=invoice_id)
         status = invoice[0].status
         if status == 'paid':
-            user = await get_user(callback.message.from_user.id)
+            user = await get_user(userid)
             user.drug_count = round(user.drug_count + int(value), 1)
             await user.save()
             await bot.send_message(callback.message.chat.id,
                                     f"✅ Ты крут бро ты получил {value} грам грамм мефа, спасибо за поддержку!")
             await Invoices.create(invoice_id=invoice_id, status='paid')
             await callback.answer()
+            await bot.send_message(LOGS_CHAT_ID, 
+                                   f'#BUYMEPH\n\n'
+                                   f'userid: <code>{userid}</code>\n'
+                                   f'amount: {value}\n'
+                                   f'invoice_id: {invoice_id}\n\n'
+                                   f'<a href="tg://user?id={userid}">mention user</a>', parse_mode='html')
         else:
             await callback.answer('❌ Оплата не найдена!')
         return
