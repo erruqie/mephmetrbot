4aa94e4d9f09c0a3b95520bd31ced314f24cfdd6
Denis <135858911+imm4d3@users.noreply.github.com>
2024-07-28 23:25:31 +0300

    fixed cryptobot pay (#9)
    Co-authored-by: Maxim <clownl3ss@icloud.com>

--- mephmetrbot/handlers/cryptopay.py
+++ mephmetrbot/handlers/cryptopay.py
@@ -1,49 +1,56 @@
-from pyCryptoPayAPI import pyCryptoPayAPI, pyCryptoPayException
+from aiocryptopay import AioCryptoPay, Networks
 from mephmetrbot.handlers.models import Users, Invoices
 from mephmetrbot.config import bot, ADMINS, RESTART_COMMAND, LOGS_CHAT_ID, CRYPTO_PAY_TOKEN
 from aiogram import Router
-from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.filters.command import Command, CommandObject
+import asyncio
 
 
-client = pyCryptoPayAPI(api_token=CRYPTO_PAY_TOKEN, print_errors=True)
 router = Router()
 
-@router.message(Command('buymeph'))
-async def buymeph_command(message: Message):
-    invoice = client.create_invoice(
-        "TON",
-        0.2,
-        description="100 единиц игровой валюты",
-        paid_btn_name="callback",
-        paid_btn_url="https://help.crypt.bot/crypto-pay-api",
-        allow_comments=True,
-        allow_anonymous=True,
-        expires_in='300'
-    )
-    await message.reply(f'💰 Вы можете купить 100 гр. за 0.2 TON\n\n[Нажмите здесь чтобы оплатить через CryptoBot]({invoice['pay_url']})\n\n[Нажмите здесь чтобы проверить оплату](https://t.me/mephmetrbot?start=checkinvoice-{invoice['invoice_id']})',parse_mode='markdown',disable_webpage_preview=True)
-    await Invoices.create(invoice_id=invoice['invoice_id'], user_id=message.from_user.id, amount_ton=0.2, amount_meph=100, status='active')
+async def get_user(user_id):
+    user, _ = await Users.get_or_create(id=user_id)
+    return user
 
-async def checkinvoice(invoice_id, message: Message):
-    invoices = client.get_invoices(
-        "TON",
-        status="paid",
-        offset=0,
-        count=10,
-        return_items = True,
-        invoice_ids = invoice_id
-    )
-    if invoices:
-        try:
-            invoice = await Invoices.get(invoice_id=invoice_id)
-        except:
-            await message.reply(f"❌ Этот счёт не найден")
-            return
-        await message.reply(f"✅ Ты крут бро ты получил {invoice.amount_meph} грамм мефа, спасибо за поддержку!")
-        user = await Users.get(id=message.from_user.id)
-        new_balance = round(user.drug_count + invoice.amount_meph, 1)
-        user.drug_count = new_balance
-        await user.save()
-        await Invoices.filter(invoice_id=invoice_id).delete()
-    else:
-        await message.reply(f"❌ Оплата не найдена")
+
+@router.message(Command('buymef'))
+async def buymef(message: Message):
+    try:
+        crypto = AioCryptoPay(token=CRYPTO_PAY_TOKEN, network=Networks.MAIN_NET)
+        invoice = await crypto.create_invoice(
+            asset='TON',
+            amount=0.2,
+            description='100 единиц игровой валюты',
+            paid_btn_name='callback',
+            paid_btn_url='https://help.crypt.bot/crypto-pay-api',
+            allow_comments=True,
+            allow_anonymous=True,
+            expires_in='300'
+        )
+
+        keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="Оплатить", url=invoice.bot_invoice_url)]
+        ])
+        await message.reply(
+            '💰 Вы можете купить 100 гр. за 0.2 TON\n\n',
+            parse_mode='markdown',
+            disable_webpage_preview=True,
+            reply_markup=keyboard
+        )
+
+        await asyncio.sleep(5)
+        old_invoice = await crypto.get_invoices(invoice_ids=[invoice.invoice_id])
+        status = old_invoice[0].status
+
+        if status == 'paid':
+            user = await get_user(message.from_user.id)
+            user.drug_count = round(user.drug_count + 100, 1)
+            await user.save()
+            await bot.send_message(message.chat.id,
+                                   "✅ Ты крут бро ты получил 100 грам грамм мефа, спасибо за поддержку!")
+
+        return invoice.bot_invoice_url, invoice.invoice_id
+
+    except Exception as e:
+        await message.bot.send_message(LOGS_CHAT_ID, f"#SENDERROR\n\nuser_id: {user_id}\nerror: {str(e)}")
